a59206f0620500cd33c62f8392087e9d07c79b88
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-05-14 10:53:20 -0300

    Criando o método de criar curso no CourseController

--- crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.loiane.crudspring.model.Course;
 import com.loiane.crudspring.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,11 @@ public class CourseController {
     @GetMapping
     public List<Course> lista() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    //@ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.courseRepository.save(course));
     }
 }
